705b76669dcab1274fee0e377a58cbe63426af79
joneilson pereira <joneilson84@gmail.com>
2024-05-01 16:44:58 -0300

    feat: convertendo retorno da lista de usuários

--- src/main/java/com/mballem/demoparkapi/web/dto/mapper/UsuarioMapper.java
+++ src/main/java/com/mballem/demoparkapi/web/dto/mapper/UsuarioMapper.java
@@ -7,6 +7,9 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
 import org.springframework.beans.BeanUtils;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UsuarioMapper {
     public static Usuario toUsuario(UsuarioCreateDto createDto) {
         Usuario user = new Usuario();
@@ -17,5 +20,9 @@ public class UsuarioMapper {
     public static UsuarioResponseDto toDto(Usuario usuario) {
         String role = usuario.getRole().name().substring("ROLE_".length());
         return new UsuarioResponseDto(usuario, role);
+    }
+
+    public static List<UsuarioResponseDto> toListDto(List<Usuario> usuarios) {
+        return usuarios.stream().map(user -> toDto(user)).collect(Collectors.toList());
     }
 }
